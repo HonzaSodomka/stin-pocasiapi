6acb5a5ded06430825008ce096c388311e857d63
HonzaSodomka <jan.sodomka@tul.cz>
2024-05-11 15:07:01 +0200

    add pay

--- stin-backend/stin-backend/Models/User.cs
+++ stin-backend/stin-backend/Models/User.cs
@@ -5,5 +5,6 @@
         public Int32 id { get; set; }
         public string? username { get; set; }
         public string? password { get; set; }
+        public string? pay {  get; set; }
     }
 }
