323936843f06c3e34d90c3819c60646bb31ffb5c
HonzaSodomka <jan.sodomka@tul.cz>
2024-05-17 22:11:23 +0200

    fix weather controller

--- stin-backend/stin-backend/Program.cs
+++ stin-backend/stin-backend/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddHttpClient();
 builder.Services.AddDbContext<DatabaseContext>(options =>
 options.UseSqlServer(
 builder.Configuration.GetConnectionString("DefaultConnection")
